44120ccdd29f642ac709b28ff4cde3fc7a342844
shivamk-tech <skkumarsv786@gmail.com>
2026-03-11 07:54:00 +0530

    updated

--- frontend/src/components/Ride/DriversRideDashboard.jsx
+++ frontend/src/components/Ride/DriversRideDashboard.jsx
@@ -55,6 +55,8 @@ const DriversRideDashboard = ({ ride, setRide }) => {
       }).addTo(mapRef.current);
     }
 
+
+    
     if (routingControlRef.current) {
       if (Array.isArray(routingControlRef.current)) {
         routingControlRef.current.forEach(control => mapRef.current.removeControl(control));
